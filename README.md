26637d2566187e3c1bc6482777a9d4a33847f36f
alexivxii <alex.munteanu58@gmail.com>
2024-03-30 14:57:56 +0200

    added better logic in main for the app flow and good regex for japan addresses

--- README.md
+++ README.md
@@ -5,6 +5,22 @@
     3          sk4designs.com
     4      draftingdesign.com
 
+Abordare:
+
+Cautare citire fisier parquet
+
+Cautare format adrese utilizand regex
+
+Testare cateva regex uri pentru US pe site ul de la google about + offices
+
+Gasire tari cu format diferit de adresa
+
+Testare
+
+Imbunatatire cod
+
+Cautare format adresa pentur japonia
+
 https://about.google/contact-google/
 
 https://www.geeksforgeeks.org/python-regex-re-search-vs-re-findall/
@@ -24,4 +40,16 @@ https://stackoverflow.com/questions/37745801/how-can-i-extract-address-from-raw-
 https://pypi.org/project/pyap/
 
 https://devnet.superoffice.com/en/company/learn/address-formats.html
+
+https://www.linkedin.com/pulse/address-cleansing-fuzzy-matching-levenshtein-distance-akhil/
+
+https://pypi.org/project/pycountry/
+
+https://www.sljfaq.org/afaq/addresses.html#:~:text=Writing%20the%20address%20in%20Japanese&text=150%2D2345%20Tokyo%2Dto%2C,7%22%20is%20the%20building%20number.
+
+https://www.kapital.jp/shop/search.html
+
+https://www.starbucksreserve.com/locations/tokyo-roastery
+
+https://pyshark.com/get-domain-name-information-using-python/
 
--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import pandas
 import requests
 from bs4 import BeautifulSoup
 import re
+import whois
 
 addressPatterns = [
     re.compile(r'\b\d{1,5}\s\w+\s\w+\b'),
@@ -21,48 +22,83 @@ addressPatterns = [
 ]
 
 addressPatternMap = {
-    "USA": re.compile(r'\b\d{1,5}\s\w+\s\w+\b.*\b[A-Za-z]{2}\s\d{5}(?:-\d{4})?\b'),
+    "RESTWORLD": re.compile(r'\b\d{1,5}\s\w+\s\w+\b.*\b[A-Za-z]{2}\s\d{5}(?:-\d{4})?\b'),
 
-    "USA2": re.compile(r'\b(?:[A-Z][a-z]+\s*)+,\s*[A-Z][a-z]+\s*,\s*[A-Z]{2}\s*\d{5}(?:-\d{4})?,\s*[A-Z][a-z]+\b'),
-    "France": re.compile(r'\b(?:[A-Z][a-z]+\s*)+,.*?,\s*\d{5}\s*[A-Z][a-z]+\b'),
+    #"USA2": re.compile(r'\b(?:[A-Z][a-z]+\s*)+,\s*[A-Z][a-z]+\s*,\s*[A-Z]{2}\s*\d{5}(?:-\d{4})?,\s*[A-Z][a-z]+\b'),
+    "FR": re.compile(r'\b(?:[A-Z][a-z]+\s*)+,.*?,\s*\d{5}\s*[A-Z][a-z]+\b'),
     "UK": re.compile(r'\b(?:[A-Z][a-z]+\s*)+:.*?,\s*[A-Z][a-z]+\s*,\s*[A-Z][a-z]+\b'),
-    # "Japan": re.compile(r'\b\d{1,5}\s\w+\s\w+\b.*\b[A-Za-z]{2}\s\d{5}(?:-\d{4})?\b'),
-    "Columbia": re.compile(r'\b\d{1,5}\s\w+\s\w+\b.*\b[A-Za-z]{2}\s\d{5}(?:-\d{4})?\b'),
-    "USAforum": re.compile(r'^(\d+) ?([A-Za-z](?= ))? (.*?) ([^ ]+?) ?((?<= )APT)? ?((?<= )\d*)?$'),
-    # "Romania": re.compile(r'\bStr\.\s+[A-Z][a-z]+\s+\d+(?:[\s,-]+\w+)?(?:,\s+et\.\s*\d+)?(?:,\s+apt\.\s*\d+)?(?:,\s*[1-6])?,?\s+\d{6}\s+[A-Z]+(?:,\s+[A-Z][a-z]+)?\b'),
 
+    #has prefecture in front
+    "JP": re.compile(r'\b(?:東京都|大阪府|福岡県|札幌市|横浜市)?\s*\d+[A-Z\d-]*[\s,-]+(?:[A-Za-z]+[\s,-]+)+\b(?:[A-Za-z]+\s)?\d{3}-\d{4}\b'),
+
+    #Todo: keep -> "JP no optional prefecture in front": re.compile(r'\b\d+[A-Z\d-]*[\s,-]+(?:[A-Za-z]+[\s,-]+)+\d{3}-\d{4}\b'),
+    "CO": re.compile(r'\b\d{1,5}\s\w+\s\w+\b.*\b[A-Za-z]{2}\s\d{5}(?:-\d{4})?\b'),
+
+    #"USAforum": re.compile(r'^(\d+) ?([A-Za-z](?= ))? (.*?) ([^ ]+?) ?((?<= )APT)? ?((?<= )\d*)?$'),
+    # "Romania": re.compile(r'\bStr\.\s+[A-Z][a-z]+\s+\d+(?:[\s,-]+\w+)?(?:,\s+et\.\s*\d+)?(?:,\s+apt\.\s*\d+)?(?:,\s*[1-6])?,?\s+\d{6}\s+[A-Z]+(?:,\s+[A-Z][a-z]+)?\b'),
 
 }
 
 
+japanAddressList = [
+
+#addresses of some stores in japan
+"1-7-2 Minami Nijo Nishi, Chuo-ku, Sapporo 060-0062",
+"1F Gaiya Ebisu Building, 2-20-2 Ebisu Minami, Shibuya-ku, 150-0022",
+"352 Iseyamachi, Rokkaku-shita, Miyukimachi-dori, Nakagyo-ku, Kyoto 604-8066",
+"5F Hankyu Men's Osaka 7-10 Kakuda-cho, Kita-ku, Osaka 530-0017",
+"3672-10 Kojima Ogawacho, Kurashiki 711-0912",
+
+"札幌市    7-10 Kakuda-cho, Kita-ku, Osaka 530-0017",
+
+#List of japanese stations addresses of Tokyo, Kyoto and Osaka for testing japan regex
+"4-chōme-2-8 Shibakōen, Minato City, Tokyo 105-0011, Japan",
+
+"Higashishiokoji Kamadonocho, Shimogyo Ward, Kyoto, Japan",
+
+"2-chōme-19-23 Aobadai Meguro City, Tokyo 153-0042, Japan",
+]
+
 # Function to scrape a webpage and check for addresses
-def check_website_for_address(url):
+#if response status code is 200 successful, we return the text inside the html
+#else we return ""
+def checkWebsiteForText(url):
     # Fetch the webpage
     response = requests.get(url)
     if response.status_code == 200:
         # Parse HTML content
         soup = BeautifulSoup(response.content, 'html.parser')
-        # Extract text from HTML and join lines into one
+        # Extract text from HTML and join lines into one, separated by whitespace
         text = " ".join(line.strip() for line in soup.stripped_strings)
+        print("Website has text")
+        return text
+
         # Check for address patterns and print addresses if found
-        print_addresses(text)
+        #print_addresses(text)
     else:
         print("Failed to fetch webpage:", url)
+        return ""
 
 
 # Function to find and print all addresses in a string
-def print_addresses(text):
-    addresses_found = False
-    for pattern in addressPatternMap:
-        print(pattern) #key
-        addresses = addressPatternMap[pattern].findall(text)
-        if addresses:
-            addresses_found = True
-            print("Addresses found:")
-            for address in addresses:
-                print("-", address)
-    if not addresses_found:
+def printAddresses(text,domainExtension):
+    addressesFound = False
+
+    pattern = addressPatternMap[domainExtension]
+
+    addresses = pattern.findall(text)
+    if addresses:
+        addressesFound = True
+        print("Addresses found:")
+        for address in addresses:
+            print("-", address)
+
+    if addressesFound is False:
         print("No address found.")
+
+def printJapanAddress():
+    for address in japanAddressList:
+        printAddresses(address, "JPprefecture")
 
 
 def readParquetFile(file_path):
@@ -89,16 +125,81 @@ def readParquetFile(file_path):
     # Now you can work with the DataFrame as usual
     print(df.head())
 
-    print(df.at[0, "domain"])
+    #print(df.at[0, "domain"])
+
+    return df
+
+def checkValidDomain(domain):
+    try:
+        domain_info = whois.whois(domain)
+        print("Domain Valid")
+        return True
+    except:
+        print("Domain Not Valid")
+        return False
+
+
+# if the website is valid and registered on the web, then the extension is also a valid one
+def getWebsiteDomainExtension(domain):
+    # Regular expression pattern to match the domain extension (. followed by at least two characters and followd by /|\?|$)
+    #tested on
+    # text1 = "www.website.com"
+    # text2 = "www.website.ro?about.html"
+    # text3 = "www.curs.pub.ro/about.html"
+    # if website is https://about.google/contact-google/, the domain extension is google, but we can still continue
+    domain_extension_pattern = r'\.([a-zA-Z]{2,})(?=/|\?|$)'
+    match = re.search(domain_extension_pattern, domain)
+    if match:
+        return match.group(1)
+    else:
+        return None
 
 
 if __name__ == '__main__':
     # Specify the path to your Parquet file
     file_path = "list of company websites.snappy.parquet"
 
-    readParquetFile(file_path)
+    pqData = readParquetFile(file_path)
+
+    #check if we have data in parquet file
+    if pqData is not None:
+        #Todo: replace testWebsite with for each website in pq file
+
+        #testWebsite = pqData.at[0, "domain"]
+        testWebsite = "https://about.google/contact-google/"
+
+        #check if the website domain is valid
+        if checkValidDomain(testWebsite):
+
+            #get text inside website
+            websiteText = checkWebsiteForText(testWebsite)
+
+            #check if the website has text inside
+            if websiteText != "":
+
+                #get website domain extension
+                domainExtension = getWebsiteDomainExtension(testWebsite)
+                print("Domain = " + domainExtension)
+
+                domainExtension = domainExtension.upper()
+
+                specialCases = ["FR", "JP", "UK", "CO"]
+
+                if domainExtension not in specialCases:
+                    domainExtension = "RESTWORLD"
+
+                # io, net, com websites are usually used for USA websites
+                # rest of the world usually uses same address format as USA, so we can cover most address formats
+                # apply correct regex pattern for address matching
+
+                printAddresses(websiteText, domainExtension)
+                printJapanAddress()
 
     # check_website_for_address("https://www.umbrawindowtinting.com/")
-    check_website_for_address("https://about.google/contact-google/")
+    #check_website_for_address("https://about.google/contact-google/")
     # check_website_for_address("https://about.google/locations/?region=north-america")
     # check_website_for_address("https://carrefour.ro/corporate/magazine?p=3")
+
+    #printJapanAddress()
+
+
